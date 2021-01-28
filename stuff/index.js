ffb02638b3a6fb23d57c580806d610ded9c746bc
Insignificant_Guy <zakramisaway@gmail.com>
2021-01-29 03:19:23 +0530

    Modified frontend design for tablet

--- stuff/index.js
+++ stuff/index.js
@@ -2,7 +2,7 @@ $( "#connect" ).click(function() {
     $( "#loader" ).toggle();
 });
 
-var time = new Date().getTime();
+/*var time = new Date().getTime();
      $(document.body).bind("mousemove keypress", function(e) {
          time = new Date().getTime();
      });
@@ -14,4 +14,4 @@ var time = new Date().getTime();
              setTimeout(refresh, 7000);
      }
 
-setTimeout(refresh, 7000);
+setTimeout(refresh, 7000);*/
